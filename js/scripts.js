999aa38491728633a7e1441a5f0b3b438b23d114
ian-bravo <bravo.ian@gmail.com>
2023-08-14 14:27:22 -0700

    finish
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com> Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com> Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", formLoader);
 
+
 function formLoader () {
-    let userForm = document.getElementById("user-form");
+    userForm = document.getElementById("user-form");
     userForm.addEventListener("submit", dataProcessor);
 }
 
@@ -10,10 +11,37 @@ function dataProcessor (event) {
 
     // Collect user input from form
     const userEntryOne = document.getElementById("favThingOne").value;
-
+    const userEntryTwo = document.getElementById("favThingTwo").value;
+    const userEntryThree = document.getElementById("favThingThree").value;
+    
     // Store in an array.
     const userDataArray = [];
     userDataArray.push(userEntryOne);
+    userDataArray.push(userEntryTwo);
+    userDataArray.push(userEntryThree);
 
     console.log("Current array: " + userDataArray);
+    console.log("2nd thing " + userDataArray[1]);
+    console.log("1st thing " + userDataArray[0]);
+    console.log("3rd thing " + userDataArray[2]);
+
+    const newArray = [];
+    newArray.push(userDataArray[1], userDataArray[0], userDataArray[2]);
+    console.log(newArray);
+
+    const ulElement = document.createElement("ul");
+    const liElementOne = document.createElement("li");
+    const liElementTwo = document.createElement("li");
+    const liElementThree = document.createElement("li");
+
+    userForm.after(ulElement);
+    ulElement.prepend(liElementOne);
+    liElementOne.after(liElementTwo);
+    ulElement.append(liElementThree);
+
+    liElementOne.append(newArray[0]);
+    liElementTwo.append(newArray[1]);
+    liElementThree.append(newArray[2]);
+    console.log(newArray);
+
 }
